bb72810bee9b3ccc73783011066ef45e59cac650
rigaud_b <matthias.rigaud@epitech.eu>
2018-08-11 01:47:05 +0200

    base compiling code

---
+++ source/Main.cpp
@@ -0,0 +1,24 @@
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+int main()
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        // Clear screen
+        window.clear();
+        // Update the window
+        window.display();
+    }
+    return EXIT_SUCCESS;
+}
